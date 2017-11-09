73f21fd19371bab165133170030d85dea703cf74
Romain Briche <romain.briche@gmail.com>
2017-11-09 14:34:29 +0100

    Fix integration imports

--- ios/RNAnalyticsSegmentIO/RNAIntegrations.h
+++ ios/RNAnalyticsSegmentIO/RNAIntegrations.h
@@ -5,72 +5,114 @@
 //  Copyright © 2017 LEO Pharma A/S. All rights reserved.
 //
 
-#if __has_include("SEGTaplyticsIntegrationFactory.h")
+#if __has_include(<Segment-Taplytics/SEGTaplyticsIntegrationFactory.h>)
 #import <Segment-Taplytics/SEGTaplyticsIntegrationFactory.h>
+#define SEGTaplyticsIntegrationFactoryImported
+#elif __has_include("SEGTaplyticsIntegrationFactory.h")
+#import "SEGTaplyticsIntegrationFactory.h"
 #define SEGTaplyticsIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGAdjustIntegrationFactory.h")
+#if __has_include(<Segment-Adjust/SEGAdjustIntegrationFactory.h>)
 #import <Segment-Adjust/SEGAdjustIntegrationFactory.h>
+#define SEGAdjustIntegrationFactoryImported
+#elif __has_include("SEGAdjustIntegrationFactory.h")
+#import "SEGAdjustIntegrationFactory.h"
 #define SEGAdjustIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGGoogleAnalyticsIntegrationFactory.h")
+#if __has_include(<Segment-GoogleAnalytics/SEGGoogleAnalyticsIntegrationFactory.h>)
 #import <Segment-GoogleAnalytics/SEGGoogleAnalyticsIntegrationFactory.h>
+#define SEGGoogleAnalyticsIntegrationFactoryImported
+#elif __has_include("SEGGoogleAnalyticsIntegrationFactory.h")
+#import "SEGGoogleAnalyticsIntegrationFactory.h"
 #define SEGGoogleAnalyticsIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGComScoreIntegrationFactory.h")
+#if __has_include(<Segment-ComScore/SEGComScoreIntegrationFactory.h>)
 #import <Segment-ComScore/SEGComScoreIntegrationFactory.h>
+#define SEGComScoreIntegrationFactoryImported
+#elif __has_include("SEGComScoreIntegrationFactory.h")
+#import "SEGComScoreIntegrationFactory.h"
 #define SEGComScoreIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGAmplitudeIntegrationFactory.h")
+#if __has_include(<Segment-Amplitude/SEGAmplitudeIntegrationFactory.h>)
 #import <Segment-Amplitude/SEGAmplitudeIntegrationFactory.h>
+#define SEGAmplitudeIntegrationFactoryImported
+#elif __has_include("SEGAmplitudeIntegrationFactory.h")
+#import "SEGAmplitudeIntegrationFactory.h"
 #define SEGAmplitudeIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGFacebookAppEventsIntegrationFactory.h")
+#if __has_include(<Segment-Facebook-App-Events/SEGFacebookAppEventsIntegrationFactory.h>)
 #import <Segment-Facebook-App-Events/SEGFacebookAppEventsIntegrationFactory.h>
+#define SEGFacebookAppEventsIntegrationFactoryImported
+#elif __has_include("SEGFacebookAppEventsIntegrationFactory.h")
+#import "SEGFacebookAppEventsIntegrationFactory.h"
 #define SEGFacebookAppEventsIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGMixpanelIntegrationFactory.h")
+#if __has_include(<Segment-Mixpanel/SEGMixpanelIntegrationFactory.h>)
 #import <Segment-Mixpanel/SEGMixpanelIntegrationFactory.h>
+#define SEGMixpanelIntegrationFactoryImported
+#elif __has_include("SEGMixpanelIntegrationFactory.h")
+#import "SEGMixpanelIntegrationFactory.h"
 #define SEGMixpanelIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGLocalyticsIntegrationFactory.h")
+#if __has_include(<Segment-Localytics/SEGLocalyticsIntegrationFactory.h>)
 #import <Segment-Localytics/SEGLocalyticsIntegrationFactory.h>
+#define SEGLocalyticsIntegrationFactoryImported
+#elif __has_include("SEGLocalyticsIntegrationFactory.h")
+#import "SEGLocalyticsIntegrationFactory.h"
 #define SEGLocalyticsIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGFlurryIntegrationFactory.h")
+#if __has_include(<Segment-Flurry/SEGFlurryIntegrationFactory.h>)
 #import <Segment-Flurry/SEGFlurryIntegrationFactory.h>
+#define SEGFlurryIntegrationFactoryImported
+#elif __has_include("SEGFlurryIntegrationFactory.h")
+#import "SEGFlurryIntegrationFactory.h"
 #define SEGFlurryIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGQuantcastIntegrationFactory.h")
+#if __has_include(<Segment-Quantcast/SEGQuantcastIntegrationFactory.h>)
 #import <Segment-Quantcast/SEGQuantcastIntegrationFactory.h>
+#define SEGQuantcastIntegrationFactoryImported
+#elif __has_include("SEGQuantcastIntegrationFactory.h")
+#import "SEGQuantcastIntegrationFactory.h"
 #define SEGQuantcastIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGCrittercismIntegrationFactory.h")
+#if __has_include(<Segment-Crittercism/SEGCrittercismIntegrationFactory.h>)
 #import <Segment-Crittercism/SEGCrittercismIntegrationFactory.h>
+#define SEGCrittercismIntegrationFactoryImported
+#elif __has_include("SEGCrittercismIntegrationFactory.h")
+#import "SEGCrittercismIntegrationFactory.h"
 #define SEGCrittercismIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGFirebaseIntegrationFactory.h")
+#if __has_include(<Segment-Firebase/SEGFirebaseIntegrationFactory.h>)
 #import <Segment-Firebase/SEGFirebaseIntegrationFactory.h>
+#define SEGFirebaseIntegrationFactoryImported
+#elif __has_include("SEGFirebaseIntegrationFactory.h")
+#import "SEGFirebaseIntegrationFactory.h"
 #define SEGFirebaseIntegrationFactoryImported
 #endif
 
-#if __has_include("SEGAppsFlyerIntegrationFactory.h")
+#if __has_include(<segment-appsflyer-ios/SEGAppsFlyerIntegrationFactory.h>)
 #import <segment-appsflyer-ios/SEGAppsFlyerIntegrationFactory.h>
+#define SEGAppsFlyerIntegrationFactoryImported
+#elif __has_include("SEGAppsFlyerIntegrationFactory.h")
+#import "SEGAppsFlyerIntegrationFactory.h"
 #define SEGAppsFlyerIntegrationFactoryImported
 #endif
 
-#if __has_include("BNCBranchIntegrationFactory.h")
+#if __has_include(<Segment-Branch/BNCBranchIntegrationFactory.h>)
 #import <Segment-Branch/BNCBranchIntegrationFactory.h>
+#define BNCBranchIntegrationFactoryImported
+#elif __has_include("BNCBranchIntegrationFactory.h")
+#import "BNCBranchIntegrationFactory.h"
 #define BNCBranchIntegrationFactoryImported
 #endif
