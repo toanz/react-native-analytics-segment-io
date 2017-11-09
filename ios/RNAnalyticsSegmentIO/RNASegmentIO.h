c01a313108c54b4438bceb1c165a124b4d6d906d
Romain Briche <romain.briche@gmail.com>
2017-11-09 14:44:28 +0100

    Fix dependency imports

--- ios/RNAnalyticsSegmentIO/RNASegmentIO.h
+++ ios/RNAnalyticsSegmentIO/RNASegmentIO.h
@@ -5,7 +5,13 @@
 //  Copyright © 2017 LEO Pharma A/S. All rights reserved.
 //
 
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import "React/RCTBridgeModule.h"
+#endif
 
 @interface RNASegmentIO : NSObject <RCTBridgeModule>
 
