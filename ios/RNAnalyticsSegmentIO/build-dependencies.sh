513544fc7e3a03fd00b1cd91d36d4b6efeb97f7d
Romain Briche <romain.briche@gmail.com>
2017-12-06 17:21:49 +0100

    Build dependencies using main workspace instead of pods project

--- ios/RNAnalyticsSegmentIO/build-dependencies.sh
+++ ios/RNAnalyticsSegmentIO/build-dependencies.sh
@@ -7,17 +7,17 @@
 
 # If the Podfile has the `use_frameworks!` flag enabled, the <Product_Name>-umbrella.h will only be resolved after the product has been compiled
 
-pods_project=${MAIN_PODS_ROOT}/Pods.xcodeproj
+main_workspace=$(find ${MAIN_SRCROOT} -name "*.xcworkspace")
 
 # Get the list of folders and schemes in Pods project with name matching one of the Segment's supported integration
 segment_regex="(^analytics$)|segment|taplytics|adjust|googleanalytics|googleidfa|comscore|amplitude|fbsdk|mixpanel|localytics|flurry|quantcast|crittercism|firebase|appsflyer|branch"
 segment_pods=$(ls ${MAIN_PODS_ROOT} | grep -iE ${segment_regex} | sed "s,/$,,")
-segment_schemes=$(xcodebuild -project ${pods_project} -list | sed '1,/Schemes:/ d' | sed 's/^ *//; /^$/d' | grep -iE ${segment_regex})
+segment_schemes=$(xcodebuild -workspace ${main_workspace} -list | sed '1,/Schemes:/ d' | sed 's/^ *//; /^$/d' | grep -iE ${segment_regex})
 
 for pod in ${segment_pods}; do
     if [[ ${segment_schemes[*]} =~ ${pod} ]]; then
         echo "Compile '${pod}'"
-        xcodebuild -project ${pods_project} -scheme ${pod} -arch ${CURRENT_ARCH} -sdk ${SDK_NAME} -configuration ${CONFIGURATION} ONLY_ACTIVE_ARCH=NO
+        xcodebuild -workspace ${main_workspace} -scheme ${pod} -arch ${CURRENT_ARCH} -sdk ${SDK_NAME} -configuration ${CONFIGURATION} ONLY_ACTIVE_ARCH=NO
     else
         echo "Copy headers '${pod}'"
         mkdir -p ${SEGMENT_HEADERS_PATH}
