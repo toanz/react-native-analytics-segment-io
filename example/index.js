94cfdd253525e66b97b637e0c2658db44f6616bc
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2018-01-24 13:46:53 +0100

    Update example app setup options
    It's useful to enable tracking of lifecycle events in the example app, because those are disabled by default and most people will want these enabled. Hopefully having them explicitly set in the example app will make it clear to people that it needs to be done manually.

--- example/index.js
+++ example/index.js
@@ -9,10 +9,13 @@ import Analytics, { AnalyticsConstants } from 'react-native-analytics-segment-io
 
 class example extends Component {
   componentDidMount() {
-    Analytics.setup('add-segment-key-here', { [AnalyticsConstants.enableAdvertisingTracking]: true, [AnalyticsConstants.debug]: true })
-      .then(() => {
-        Analytics.track('test', {})
-      })
+    Analytics.setup('add-segment-key-here', {
+      [AnalyticsConstants.trackApplicationLifecycleEvents]: true,
+      [AnalyticsConstants.debug]: true
+    })
+    .then(() => {
+      Analytics.track('test', {})
+    })
   }
 
   render() {
